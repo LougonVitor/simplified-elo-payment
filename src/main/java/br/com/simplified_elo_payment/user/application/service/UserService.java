ade23b84d4de8144024d651c9a7a0fd157fd5c30
Vítor Lougon <vitor.lougon@gmail.com>
2026-02-17 12:09:33 -0300

    feat: triggering rest event

--- src/main/java/br/com/simplified_elo_payment/user/application/service/UserService.java
+++ src/main/java/br/com/simplified_elo_payment/user/application/service/UserService.java
@@ -3,20 +3,27 @@ package br.com.simplified_elo_payment.user.application.service;
 import br.com.simplified_elo_payment.user.application.dto.CreateUserCommand;
 import br.com.simplified_elo_payment.user.application.dto.UserServiceResponseDto;
 import br.com.simplified_elo_payment.user.domain.entity.UserEntity;
+import br.com.simplified_elo_payment.user.domain.port.IPortAccountProvider;
 import br.com.simplified_elo_payment.user.domain.repository.IUserRepository;
 import br.com.simplified_elo_payment.user.domain.valueobject.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
 public class UserService {
     @Autowired
     private IUserRepository iUserRepository;
+    @Autowired
+    private IPortAccountProvider iPortAccountProvider;
 
     public UserServiceResponseDto createUser(CreateUserCommand createUserCommand) {
         UserEntity userEntity = new UserEntity(createUserCommand.username(), createUserCommand.email(), createUserCommand.password(), UserRole.valueOf(createUserCommand.role()));
 
         UserEntity response = this.iUserRepository.createUser(userEntity);
+
+        this.iPortAccountProvider.creationAccountEvent(response.getId(), new BigDecimal(100));
 
         return new UserServiceResponseDto(response.getId(), response.getUsername(), response.getEmail());
     }
