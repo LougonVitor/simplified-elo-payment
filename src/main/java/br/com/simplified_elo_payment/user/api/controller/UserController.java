3936643eb62fb6065fd3e716f1d87a2a25e7e8cb
Vítor Lougon <vitor.lougon@gmail.com>
2026-02-17 09:42:29 -0300

    fix: correcting endpoint mapping

--- src/main/java/br/com/simplified_elo_payment/user/api/controller/UserController.java
+++ src/main/java/br/com/simplified_elo_payment/user/api/controller/UserController.java
@@ -6,16 +6,19 @@ import br.com.simplified_elo_payment.user.api.mapper.UserMapper;
 import br.com.simplified_elo_payment.user.application.dto.CreateUserCommand;
 import br.com.simplified_elo_payment.user.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    public UserResponseDto createUser(UserRequestDto request) {
+    @PostMapping
+    public UserResponseDto createUser(@RequestBody UserRequestDto request) {
         CreateUserCommand userCommand = UserMapper.toCreateUserCommand(request);
 
         return UserMapper.toResponseDto(this.userService.createUser(userCommand));
