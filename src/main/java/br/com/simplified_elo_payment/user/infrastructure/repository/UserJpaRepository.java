b66cad5ebdd71feaedc4d72309bcc6cac8e19643
Vítor Lougon <vitor.lougon@gmail.com>
2026-02-17 08:46:07 -0300

    feat: configuring the UserJpaRepository as a bean

--- src/main/java/br/com/simplified_elo_payment/user/infrastructure/repository/UserJpaRepository.java
+++ src/main/java/br/com/simplified_elo_payment/user/infrastructure/repository/UserJpaRepository.java
@@ -2,7 +2,9 @@ package br.com.simplified_elo_payment.user.infrastructure.repository;
 
 import br.com.simplified_elo_payment.user.infrastructure.entity.UserJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
     UserJpaEntity findByUsername(String username);
 }
