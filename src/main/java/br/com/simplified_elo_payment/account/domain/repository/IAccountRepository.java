8265ca7351a2c6ba9407c3d4f49ce7262cba2b70
Vítor Lougon <vitor.lougon@gmail.com>
2026-02-17 09:58:00 -0300

    feat: adding receiving user and paying user in the domain interface

--- src/main/java/br/com/simplified_elo_payment/account/domain/repository/IAccountRepository.java
+++ src/main/java/br/com/simplified_elo_payment/account/domain/repository/IAccountRepository.java
@@ -3,6 +3,6 @@ package br.com.simplified_elo_payment.account.domain.repository;
 import java.math.BigDecimal;
 
 public interface IAccountRepository {
-    BigDecimal paymentTransaction(BigDecimal amountPaid);
-    BigDecimal receiptTransaction(BigDecimal amountReceived);
+    BigDecimal paymentTransaction(BigDecimal paidAmount, Long receivingUser);
+    BigDecimal receiptTransaction(BigDecimal receivedAmount, Long payingUser);
 }
